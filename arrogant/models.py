079d05af1d6da6539bb0c396c9cdb1cdcca70de9
david <davidtnfsh@gmail.com>
2017-06-19 21:54:48 +0800

    finish preliminary design of jlist and jvalue

--- arrogant/models.py
+++ arrogant/models.py
@@ -3,27 +3,43 @@ from django.utils import timezone
 from infernoWeb.models import User
 
 # Create your models here.
+class CompanyManager(models.Manager):
+    def get_by_natural_key(self, brand):
+        return self.get(brand=brand)
+
 class Company(models.Model):
-	brand = models.CharField(max_length=30)
-	banner = models.CharField(max_length=200) # 大頭貼照片
-	path = models.CharField(max_length=40)
-	area = models.CharField(max_length=10)
-	公司規模 = models.CharField(max_length=5)
-	地址 = models.CharField(max_length=40)
-	資本額 = models.CharField(max_length=10)
-	description = models.CharField(max_length=150)
+	objects = CompanyManager()
+
+	brand = models.CharField(max_length=30, default='')
+	banner = models.CharField(max_length=200, default='') # 大頭貼照片
+	path = models.CharField(max_length=40, default='')
+	area = models.CharField(max_length=10, default='')
+	公司規模 = models.CharField(max_length=5, default='')
+	地址 = models.CharField(max_length=40, default='')
+	資本額 = models.CharField(max_length=10, default='')
+	description = models.CharField(max_length=150, default='')
 	def __str__(self):
 		return self.brand
+
+	def natural_key(self):
+	    return {
+	    	"brand":self.brand,
+	    	"banner":self.banner,
+	    	"area":self.area,
+	    }
+
+	class Meta:
+	    unique_together = ('brand', 'path')
 
 class Job(models.Model):
 	"""docstring for Job"""
 	company = models.ForeignKey(Company)
-	name = models.CharField(max_length=20)
+	name = models.CharField(max_length=20, default='')
 	intern_tf = models.BooleanField(default=False)
 	has_salary_info = models.BooleanField(default=False)
-	salary = models.CharField(max_length=30)
-	path = models.CharField(max_length=40)
-	avatar = models.CharField(max_length=200) # 大頭貼照片
+	salary = models.CharField(max_length=30, default='')
+	path = models.CharField(max_length=40, default='')
+	avatar = models.CharField(max_length=200, default='') # 大頭貼照片
 	def __str__(self):
 		return self.name
 
@@ -60,7 +76,7 @@ class Comment(models.Model):
 
 class LikesFromUser(models.Model):
     author = models.OneToOneField(User, related_name='lfuauthor')
-    comment = models.ManyToManyField(Comment)
+    comment = models.ManyToManyField(Comment)	
     def __str__(self):
         return self.author.name
 
